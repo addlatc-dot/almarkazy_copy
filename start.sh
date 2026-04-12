3939d126bf7b889334cc33891fe4e087be04edb3
Omar Eid <addlatc@gmail.com>
2026-04-12 13:03:20 +0200

    eventlet update

--- start.sh
+++ start.sh
@@ -11,4 +11,6 @@ echo "Running migrations..."
 alembic upgrade head
 
 echo "Starting server..."
-gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:${PORT:-8080} --timeout 120 app:app
+# gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:${PORT:-8080} --timeout 120 app:app
+
+gunicorn --worker-class eventlet -w 2 --bind 0.0.0.0:${PORT:-8080} --timeout 600 app:app
